96578978ce18681a1b5574ee7e0bd21ed432ac24
merten2000 <merten.van.gastel@hotmail.com>
2018-03-15 11:32:20 +0000

    added modules

--- ingenico/terraform-AWS-vpc/AWS.tf
+++
@@ -1,8 +0,0 @@
-variable "access_key_aws" {}
-variable "secret_key_aws" {}
-
-provider "aws" {
-  access_key = "${var.access_key_aws}"
-  secret_key = "${var.secret_key_aws}"
-  region     = "eu-west-3"
-}
