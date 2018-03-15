58f132bcc1b55facb3e9ac461b7a74dd14f09493
merten2000 <merten.van.gastel@hotmail.com>
2018-03-15 11:18:22 +0000

    added modules

--- Instances_bulk.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "example" {
-  count = 2
-  ami = "ami-8ee056f3"
-  instance_type = "t2.small"
-  subnet_id =  "${aws_subnet.example.id}"
-  tags {
-    Name = "example-${count.index}"
-  }
-}
